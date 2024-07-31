90dfb94f4e6fe5bdf37257f3c72f69f0d6b93b69
TimWong <timwong.tw93@gmail.com>
2024-07-31 20:43:33 +1000

    updated mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,7 +2,7 @@ import {gql} from '@apollo/client';
 
 export const ADD_USER = gql `
   mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
@@ -14,12 +14,30 @@ export const ADD_USER = gql `
 
 export const LOGIN_USER = gql `
   mutation login($email: String!, $password: String!) {
-    login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
       token
       user {
         _id
         username
       }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql `
+  mutation saveBook($authors: String!, $description: String!, $title: String!, $bookId: String!, $image: String!, $link: String!) {
+    saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link) {
+      bookId
+      title
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql `
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      bookId
+      title
     }
   }
 `;
